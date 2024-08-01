603720d1e7d917ee4e8d7aceb4b532a5b2635596
Neal Sidhwaney <nealsid@users.noreply.github.com>
2024-08-01 19:26:55 -0400

    WIP, add comments and compute filename base based on configured suffixes for a major mode

--- relfiles.el
+++ relfiles.el
@@ -1,5 +1,5 @@
 (setq relfiles-suffixes-alist
-      '((java-mode . ("test"))
+      '((java-mode . ("test" "model"))
         (c++-mode . ("_test"))))
 
 (setq relfiles-extensions-alist
@@ -15,7 +15,34 @@
         ((string-match "/javatests/" x) (file-name-directory (string-replace "/java/" "/javatests/" x)))
         (x)))
 
-(defun relfiles-compute-directories-for-filename (filename)
+(defun relfiles-compute-file-name-base (filename)
+  (let* ((fn (file-name-base filename))
+         (suffixes (alist-get major-mode relfiles-suffixes-alist))
+         (removed-suffixes
+          (cl-remove-if-not 'identity
+                            ;; For each suffix, match it against the
+                            ;; end of the filename base.  If it's a
+                            ;; match, return a new string which
+                            ;; replaces the suffix with an empty
+                            ;; string.  If it's not a match, return
+                            ;; nil.  Nil entries are removed (see
+                            ;; above remove-if-not) and the remaining
+                            ;; entry is considered the filename base,
+                            ;; or, if all entries are nil, meaning no
+                            ;; suffixes matched, take the filename as
+                            ;; passed in the argument as the base
+                            ;; filename.
+                            (mapcar (lambda (suffix)
+                                      (let ((suffix-regexp (concat suffix "$")))
+                                        (if (string-match suffix-regexp fn)
+                                            (replace-regexp-in-string suffix-regexp "" fn)
+                                          nil))) suffixes))))
+    (cond ((not removed-suffixes) fn)
+          ((nth 0 removed-suffixes)))
+    ))
+
+
+(defun relfiles-compute-search-directories-for-filename (filename)
   (with-current-buffer (find-file filename)
     (let ((file-path (file-name-directory filename))
           (relfiles-directories (alist-get major-mode relfiles-search-directories-alist)))

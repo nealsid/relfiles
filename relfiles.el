d32a6137c2eec5613a17e76168f1765c583b8222
Neal Sidhwaney <nealsid@users.noreply.github.com>
2024-08-01 15:49:44 -0400

    Add alists with per-mode settings and function to transform java pathnames into parallel test/prod tree pathnames

--- relfiles.el
+++ relfiles.el
@@ -1,19 +1,39 @@
-(setq relfiles-basename-suffixes
-      '("test" "controller" "model" "view"))
+(setq relfiles-suffixes-alist
+      '((java-mode . ("test"))
+        (c++-mode . ("_test"))))
 
-(setq relfiles-xform-alist
-      '("%project_root%/%reldir%/%basename%.el"
-        "%project_root%/%reldir%/%basename~(_test$)%.el"))
+(setq relfiles-extensions-alist
+      '((java-mode . ("java"))
+        (c++-mode . ("cc" "cpp" "cxx" "c" "h" "hpp" "hxx"))))
 
-(defun apply-xform-to-fn (fn xform)
+(setq relfiles-search-directories-alist
+      '((java-mode . (relfiles-parallel-java-tree))
+        (c++-mode . ("tests"))))
 
+(defun relfiles-parallel-java-tree (x)
+  (cond ((string-match "/java/" x) (file-name-directory (string-replace "/java/" "/javatests/" x)))
+        ((string-match "/javatests/" x) (file-name-directory (string-replace "/java/" "/javatests/" x)))
+        (x)))
+
+(defun relfiles-compute-directories-for-filename (filename)
+  (with-current-buffer (find-file filename)
+    (let ((file-path (file-name-directory filename))
+          (relfiles-directories (alist-get major-mode relfiles-search-directories-alist)))
+      (mapcar (lambda (pathname-or-function)
+                (if (functionp pathname-or-function)
+                    (apply pathname-or-function (list filename))
+                  (concat file-path pathname-or-function))) relfiles-directories))))
+
+(defun relfiles-for-filename (filename)
+  (with-current-buffer (find-file filename)
+    (let ((relfiles-exts ((alist-get major-mode relfiles-extensions-alist)))
+          (relfiles-suffixes ((alist-get major-mode relfiles-suffixes-alist)))
+          (filename-base (file-name-base filename))
+          (filename-directory (file-name-directory filename))
+          )
+
+      )
+    )
   )
-(defun transform-fn-by-xforms (fn)
-  (let      ((fn-dir (file-name-directory fn))
-        (fn-filename (file-name-nondirectory fn))
-       (fn-extension (file-name-extension fn)))
-    (mapcar)
-    ))
-
 (defun relfiles (fn)
   )

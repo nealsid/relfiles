41282dc2b7dc3f227657f9b68ddf0c2449c89e29
Neal Sidhwaney <nealsid@users.noreply.github.com>
2024-08-02 17:27:58 -0400

    Use defvar-local, and start of computing files in all search directories

--- relfiles.el
+++ relfiles.el
@@ -1,14 +1,16 @@
-(setq relfiles-suffixes-alist
-      '((java-mode . ("test" "model"))
-        (c++-mode . ("_test"))))
+(defvar-local relfiles-suffixes-alist
+          '((java-mode . ("test" "model"))
+            (c++-mode . ("_test"))))
 
-(setq relfiles-extensions-alist
+(defvar-local relfiles-extensions-alist
       '((java-mode . ("java"))
         (c++-mode . ("cc" "cpp" "cxx" "c" "h" "hpp" "hxx"))))
 
-(setq relfiles-search-directories-alist
+(defvar-local relfiles-search-directories-alist
       '((java-mode . (relfiles-parallel-java-tree))
-        (c++-mode . ("tests"))))
+        ;; Have to include trailing / to be consistent with
+        ;; default-directory variable.
+        (c++-mode . ("tests/"))))
 
 (defun relfiles-parallel-java-tree (x)
   (cond ((string-match "/java/" x) (file-name-directory (string-replace "/java/" "/javatests/" x)))
@@ -36,7 +38,8 @@
                                       (let ((suffix-regexp (concat suffix "$")))
                                         (if (string-match suffix-regexp fn)
                                             (replace-regexp-in-string suffix-regexp "" fn)
-                                          nil))) suffixes))))
+                                          nil)))
+                                    suffixes))))
     (cond ((not removed-suffixes) fn)
           ((nth 0 removed-suffixes)))
     ))
@@ -46,21 +49,20 @@
   (with-current-buffer (find-file filename)
     (let ((file-path (file-name-directory filename))
           (relfiles-directories (alist-get major-mode relfiles-search-directories-alist)))
-      (mapcar (lambda (pathname-or-function)
-                (if (functionp pathname-or-function)
-                    (apply pathname-or-function (list filename))
-                  (concat file-path pathname-or-function))) relfiles-directories))))
+      (append (mapcar (lambda (pathname-or-function)
+                        (if (functionp pathname-or-function)
+                            (apply pathname-or-function (list filename))
+                          (concat file-path pathname-or-function)))
+                      relfiles-directories)
+              (list default-directory)))))
 
 (defun relfiles-for-filename (filename)
   (with-current-buffer (find-file filename)
-    (let ((relfiles-exts ((alist-get major-mode relfiles-extensions-alist)))
-          (relfiles-suffixes ((alist-get major-mode relfiles-suffixes-alist)))
-          (filename-base (file-name-base filename))
-          (filename-directory (file-name-directory filename))
-          )
+    (let ((search-directories (relfiles-compute-search-directories-for-filename filename))
+          (filename-base (relfiles-compute-file-name-base filename)))
+      (mapcar (lambda (directory)
+                (concat directory filename-base))
+              search-directories))))
 
-      )
-    )
-  )
 (defun relfiles (fn)
   )

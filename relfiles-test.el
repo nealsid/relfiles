aa60cceb8f2a90e843a5f80ffbb3f1931395e6ef
Neal Sidhwaney <nealsid@users.noreply.github.com>
2024-08-08 15:55:03 -0400

    Update & bugfix with-project-and-directory macro and rework the test case a bit

--- relfiles-test.el
+++ relfiles-test.el
@@ -1,28 +1,38 @@
-(defmacro with-project-and-directory (project-dir-name-binding &rest forms)
+(defmacro with-test-directory (dir-name-binding &rest forms)
   "Macro to wrap a test case body that creates bindings for the
 unique project directory for the test case."
-  `(let* ((,project-dir-name-binding (concat (make-temp-file "relfiles-test-" t) "/"))
-     (progn
-       ,@forms))))
+  `(let* ((,dir-name-binding (concat (make-temp-file "relfiles-test-" t) "/")))
+     ,@forms))
 
 (defmacro with-current-buffer-close (buffer-or-name &rest body)
   `(progn
      (with-current-buffer ,buffer-or-name ,@body)
      (kill-buffer ,buffer-or-name)))
 
-(ert-deftest related-files-same-directory ()
+(ert-deftest related-files-same-directory-c++ ()
   "Opens a project with one file"
-  (with-project-and-directory
+  (with-test-directory
    project-dir
-   (let ((header-file (concat project-dir "main.h"))
-         (development-file (concat project-dir "main.cpp"))
-         (test-file (concat project-dir "main_test.cpp")))
+   (let* ((query-about-changed-file nil) ;; We touch the files after
+                                         ;; initially visiting them,
+                                         ;; so make sure Emacs doesn't
+                                         ;; ask about reverting the
+                                         ;; buffer when visiting them
+                                         ;; again later on
+          (header-file (concat project-dir "main.h"))
+          (development-file (concat project-dir "main.cpp"))
+          (test-file (concat project-dir "main_test.cpp"))
+          (header-file-buffer (find-file-noselect header-file))
+          (test-file-buffer (find-file-noselect test-file))
+          (dev-file-buffer (find-file-noselect development-file)))
      (make-empty-file header-file)
      (make-empty-file development-file)
      (make-empty-file test-file)
-     (with-current-buffer-close
-      (find-file-noselect development-file)
+
+     (with-current-buffer-close dev-file-buffer
       (relfiles-visit-related-files (buffer-file-name))
-      (should (eq (current-buffer) (get-file-buffer header-file)))
+      (should (eq (current-buffer) header-file-buffer))
       (relfiles-visit-related-files (buffer-file-name))
-      (should (eq (current-buffer) (get-file-buffer test-file)))))))
+      (should (eq (current-buffer) test-file-buffer))
+      (kill-buffer header-file-buffer)
+      (kill-buffer test-file-buffer)))))
